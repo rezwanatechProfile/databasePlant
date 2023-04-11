9ae1d1acfebc35257b6db682b2851e698fbbffb6
Rezwana Sultana <rezwanatech@gmail.com>
2023-04-11 12:29:10 -0400

    Update plantsRoutes.js

--- routes/plantsRoutes.js
+++ routes/plantsRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const { plantsCtrls } = require('../controllers') //all of our methods inside of controllers/index.js 
 
 // ROUTES - METHODS // 
-router.get('/', plantsCtrls.getPlants)
-router.get('/:id', plantsCtrls.getDetails)
+// router.get('/', plantsCtrls.getPlants)
+router.get('/', plantsCtrls.getDetails)
 
 
 module.exports = router
