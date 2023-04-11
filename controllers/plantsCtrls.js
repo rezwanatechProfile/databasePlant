e1fb0158ed3d3051132ae35e2da29011fe418497
Rezwana Sultana <rezwanatech@gmail.com>
2023-04-10 20:45:16 -0400

    Update plantsCtrls.js

--- controllers/plantsCtrls.js
+++ controllers/plantsCtrls.js
@@ -3,7 +3,7 @@
 const getPlants = (req, res) => {
   const PlantDataBase = async () => {
     // make api call and get response
-    const response = await fetch('https://perenual.com/api/species-list?page=1&key=sk-pdyK642b684e0b8c3421');
+    const response = await fetch(`https://perenual.com/api/species-list?page=${page}&key=sk-pdyK642b684e0b8c3421`);
     // turn response into javascript object
     const data = await response.json();
     // set the about state to the data
